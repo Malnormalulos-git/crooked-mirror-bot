53412f248c9d0a524ef4536cd6c3e62dbb8d7836
Malnormalulos-git <malnormalulo1@gmail.com>
2025-07-25 14:39:16 +0300

    bot setup

---
+++ config_reader.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+from pathlib import Path
+
+
+class Config(BaseSettings):
+    bot_token: SecretStr
+
+    model_config = SettingsConfigDict(
+        env_file=Path(__file__).parent / '.env'
+    )
+
+
+config = Config()
